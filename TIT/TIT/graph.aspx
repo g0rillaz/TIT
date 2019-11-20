eb338179340271dd2c70e26c59fabc136d8750b1
BuildTools <unconfigured@null.spigotmc.org>
2019-11-20 19:43:18 +0100

    Graph Page Tests

--- TIT/TIT/graph.aspx
+++ TIT/TIT/graph.aspx
@@ -2,5 +2,5 @@
 
 <%@ Register Assembly="System.Web.DataVisualization, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" Namespace="System.Web.UI.DataVisualization.Charting" TagPrefix="asp" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
-    <asp:Chart ID="chart_main" runat="server"></asp:Chart>
+    <asp:Chart ID="chart_main" Width="1000px" Height="600px" runat="server"></asp:Chart>
 </asp:Content>
--- TIT/TIT/graph.aspx.cs
+++ TIT/TIT/graph.aspx.cs
@@ -20,38 +20,81 @@ namespace TIT
 
 
             // Erstellen einer Liste und füllen dieser
-            List<WeatherData> data = new List<WeatherData>();
+            List<ChartData> list_data1 = new List<ChartData>();
+
+            list_data1.Add(new ChartData("Min", "Januar", "{0.8, 10}"));
+            list_data1.Add(new ChartData("Min", "Februar", "1.2"));
+            list_data1.Add(new ChartData("Min", "März", "4.5"));
+            list_data1.Add(new ChartData("Min", "April", "9.1"));
+            list_data1.Add(new ChartData("Min", "Mai", "13.4"));
+            list_data1.Add(new ChartData("Min", "Juni", "16.5"));
+            list_data1.Add(new ChartData("Min", "Juli", "18.6"));
+            list_data1.Add(new ChartData("Min", "August", "17.9"));
+            list_data1.Add(new ChartData("Min", "September", "14"));
+            list_data1.Add(new ChartData("Min", "Oktober", "9.6"));
+            list_data1.Add(new ChartData("Min", "November", "5.2"));
+            list_data1.Add(new ChartData("Min", "Dezember", "1.9"));
+
+            List<ChartData> list_data2 = new List<ChartData>();
+            list_data2.Add(new ChartData("Max", "Januar", "-0.5"));
+            list_data2.Add(new ChartData("Max", "Februar", "0.4"));
+            list_data2.Add(new ChartData("Max", "März", "3.5"));
+            list_data2.Add(new ChartData("Max", "April", "10"));
+            list_data2.Add(new ChartData("Max", "Mai", "15"));
+            list_data2.Add(new ChartData("Max", "Juni", "20"));
+            list_data2.Add(new ChartData("Max", "Juli", "23"));
+            list_data2.Add(new ChartData("Max", "August", "21"));
+            list_data2.Add(new ChartData("Max", "September", "12"));
+            list_data2.Add(new ChartData("Max", "Oktober", "9"));
+            list_data2.Add(new ChartData("Max", "November", "4"));
+            list_data2.Add(new ChartData("Max", "Dezember", "0.8"));
 
 
+            ChartArea ChartArea1 = new ChartArea();
+            ChartArea1.AxisX.Interval = 1;
+            ChartArea1.AxisX.IsMarginVisible = false;
 
-            double[] yValues = { 15, 60, 12, 13};
-            string[] xValues = { "September", "October", "November", "December" };
+            chart_main.ChartAreas.Add(ChartArea1);
+
+            Series Series1 = new Series();
+            Series1.IsValueShownAsLabel = true;
+            Series1.XValueType = ChartValueType.String;
+            Series1.XValueMember = "xValue";
+            Series1.YValueMembers = "yValue";
+            Series1.ChartType = SeriesChartType.SplineArea;
+            Series1.Points.DataBind(list_data1, "xValue", "yValue", "Tooltip = Group");
+            chart_main.Series.Add(Series1);
 
 
+            chart_main.Series.Add("Series2");
+            chart_main.Series["Series2"].ChartType = SeriesChartType.SplineArea;
 
+            chart_main.Series["Series2"].IsValueShownAsLabel = true;
+            chart_main.Series["Series2"].XValueType = ChartValueType.String;
+            chart_main.Series["Series2"].XValueMember = "xValue";
+            chart_main.Series["Series2"].YValueMembers = "yValue";
+            chart_main.Series["Series2"].Points.DataBind(list_data2, "xValue", "yValue", "Tooltip = Group");
+          
 
-            chart_main.Series.Add("Default1");
-            chart_main.Series.Add("Default2");
-            chart_main.Series.Add("Default3");
-            chart_main.Series.Add("Default4");
-            chart_main.Series.Add("Default5");
-
-            chart_main.ChartAreas.Add("ChartArea1");
-            chart_main.ChartAreas.Add("ChartArea2");
-
-            chart_main.Series[0].ChartArea = "ChartArea1";
-            chart_main.Series[0].ChartType = SeriesChartType.SplineArea;
-
-            chart_main.Series[1].ChartArea = "ChartArea2";
-            chart_main.Series[1].ChartType = SeriesChartType.Line;
-
-            chart_main.Series["Default1"].Points.DataBindXY(xValues, yValues);
-            chart_main.Series["Default2"].Points.DataBindXY(xValues, yValues);
         }
 
-        private class WeatherData
+        private class ChartData
         {
+            public string group { get; set; }
+            public string xValue { get; set; }
+            public string yValue { get; set; }
 
+            public ChartData()
+            {
+
+            }
+
+            public ChartData(string group,string xValue, string yValue)
+            {
+                this.group = group;
+                this.xValue = xValue;
+                this.yValue = yValue;
+            }
         }
     }
 }
